a4ce6ee56e9b3d6c1ab415e29a5ed261f6b8764d
Takreemkhan <116283240+Takreemkhan@users.noreply.github.com>
2024-09-05 17:46:33 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // MongoDB Connection URI
 //const mongoURI = 'mongodb://127.0.0.1:27017/CarInsights'; // Replace 'CarInsights' with your actual database name
 const mongoURI = "mongodb+srv://7takreemkhan7:takreem1234@carinsightdb.hxldn.mongodb.net/?retryWrites=true&w=majority&appName=CarInsightDb"; 
+                  
 // Function to connect to MongoDB
 const connectDB = async () => {
   try {
